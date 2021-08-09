5fa93cc3c3ef2af773dd3b69bb284215781d05e3
AbdulHaseeb22 <abdulhaseeb.soomro8@gmail.com>
2021-08-09 15:04:14 +0500

    hulu-clone

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -5,7 +5,7 @@ import { forwardRef } from "react";
 const Thumbnail = forwardRef(({ result }, ref) => {
     const BASE_URL = "https://image.tmdb.org/t/p/original/";
     return (
-        <div ref={ref} className="pt-2 px-2 group cursor-pointer  transitition duration-200 ease-in transform 
+        <div ref={ref} className="p-2 group cursor-pointer  transitition duration-200 ease-in transform 
         sm:hover:scale-x-105 hover:z-50">
            <Image
            className="rounded-2xl"
